cc0d48584030f64b9ff475a8558e3b26b577397a
Mark Sisson <5761292+marksisson@users.noreply.github.com>
2026-02-16 07:43:08 -0600

    update 6

--- modules/partitions.nix
+++ modules/partitions.nix
@@ -1,4 +1,9 @@
-{ inputs, lib, ... }:
+{
+  config,
+  inputs,
+  lib,
+  ...
+}:
 let
   default =
     let
@@ -6,18 +11,20 @@ let
     in
     {
       imports = [ inputs.flake-parts.flakeModules.partitions ];
-      partitions.${partition} = {
-        extraInputsFlake = ../partitions/${partition};
-        module =
-          { inputs, lib, ... }:
-          {
+      partitions.${partition} =
+        let
+          inputs = config.partitions.default.extraInputs;
+        in
+        {
+          extraInputsFlake = ../partitions/${partition};
+          module = {
             # default systems
             systems = lib.mkDefault (import inputs.systems);
 
             # default pkgs
             imports = [ inputs.pkgs.components.nixology.pkgs.nixpkgs ];
           };
-      };
+        };
     };
 
   module = default;
