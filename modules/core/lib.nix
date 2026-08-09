0c8daf572b2fbecce40f5887e6f298fc8c3e0526
Mark Sisson <5761292+marksisson@users.noreply.github.com>
2026-08-09 11:48:55 -0500

    feat(core): add component creation helper

--- modules/core/lib.nix
+++ modules/core/lib.nix
@@ -7,11 +7,14 @@ let
   inherit (local.inputs.self.components) nixology;
 
   inherit (lib)
+    all
+    assertMsg
     extend
     filter
     getAttrFromPath
     makeExtensible
     mkDefault
+    optional
     optionals
     setDefaultModuleLocation
     ;
@@ -71,6 +74,89 @@ let
           [ ];
 
       evalComponent = args: component: evalFlakeModule args component.module;
+
+      mkComponent =
+        nameOrSource: scope:
+        {
+          modules ? { },
+          dependencies ? [ ],
+          domain ? null,
+          subdomain ? null,
+        }:
+        let
+          inherit (scope.inputs) flake;
+          inherit (scope.config) flakeref;
+
+          componentName = flake.lib.getFileStem nameOrSource;
+
+          flakerefComponents =
+            let
+              match = builtins.match "([a-zA-Z0-9+.-]+):([^/]+)/([^/?#]+)(/([^?#]+))?(\\?.*)?" flakeref;
+            in
+            if match == null then
+              null
+            else
+              {
+                forge = builtins.elemAt match 0;
+                owner = builtins.elemAt match 1;
+                repo = builtins.elemAt match 2;
+                ref = builtins.elemAt match 4;
+              };
+
+          componentDomain =
+            if domain != null then
+              domain
+            else if flakerefComponents != null then
+              flakerefComponents.owner
+            else
+              abort "Unable to determine component domain from flake reference: ${flakeref}";
+
+          componentSubdomain =
+            if subdomain != null then
+              subdomain
+            else if flakerefComponents != null then
+              flake.lib.getFileStem flakerefComponents.repo
+            else
+              abort "Unable to determine component subdomain from flake reference: ${flakeref}";
+
+          featureModule = modules.flake or null;
+          targetModules = removeAttrs modules [ "flake" ];
+
+          featureTargetModules =
+            if builtins.attrNames targetModules == [ ] then
+              null
+            else
+              {
+                imports = [ flake.components.nixology.flake.modules.module ];
+                flake.modules = builtins.mapAttrs (class: module: {
+                  ${componentName} = {
+                    key = "${flakeref}#components.${componentName}";
+                    imports = [ module ];
+                    _class = class;
+                  };
+                }) targetModules;
+              };
+
+          implementation = {
+            imports =
+              optional (featureModule != null) featureModule
+              ++ optional (featureTargetModules != null) featureTargetModules;
+          };
+        in
+        assert assertMsg (all (name: builtins.elem name config.moduleClasses) (
+          builtins.attrNames modules
+        )) "modules contains an unsupported module class";
+        {
+          imports = [ implementation ];
+
+          flake.components.${componentDomain}.${componentSubdomain}.${componentName} = {
+            inherit implementation;
+            dependencies = [
+              flake.components.nixology.core.flake
+            ]
+            ++ dependencies;
+          };
+        };
 
       mkFlake =
         args: module:
@@ -142,6 +228,7 @@ let
         getFileStem
         evalComponent
         metadataForFlakeInput
+        mkComponent
         mkFlake
         mkTOMLFlake
         modulesIn
@@ -149,6 +236,7 @@ let
       components = {
         inherit
           evalComponent
+          mkComponent
           uses
           ;
       };
